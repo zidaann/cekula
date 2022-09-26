c401c2e0e0084dbc981fd8da0902c0ee54f9d1ff
zidaann <jourjizaidan8@gmail.com>
2022-09-26 12:25:44 +0700

    first-commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cekula</title>
+
+    <!-- Link CSS-->
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+  </head>
+  <body>
+    
+    <div id="main">
+        <div>
+            <x-navbar></x-navbar>
+        </div>
+    </div>
+
+
+
+    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    hai
+</x-app-layout>
