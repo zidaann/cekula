1bd4981b8f007b6c381f050496d2f4de4cd71e4c
WildanR1 <wildanrachmadoni@gmail.com>
2022-10-06 21:55:51 +0700

    tambah sidebar

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/login', function(){
     return view('login.index');
 })->name('login');
+
+Route::get('/dashboard', function(){
+    return view('dashboard.dashboard');
+})->name('dashboard');
