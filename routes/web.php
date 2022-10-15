98d7b8e1caed392bb44bb3be30d7dc993dd7d324
zidaann <jourjizaidan8@gmail.com>
2022-10-15 23:15:50 +0700

    update login

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\JadwalKelasController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Dashboard\Admin\JadwalSekolahController;
-use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
@@ -19,8 +20,15 @@ Route::get('login', [LoginController::class, 'index'])->name('login');
 Route::post('login', [LoginController::class, 'store']);
 
 Route::prefix('akademik')->group(function(){
-    Route::get('jadwal-sekolah', [JadwalSekolahController::class, 'index'])->name('jadwal_sekolah.index');
-    Route::get('create-jadwal-sekolah', [JadwalSekolahController::class, 'create'])->name('jadwal_sekolah.create');
+    Route::prefix('jadwal-sekolah')->group(function(){
+        Route::get('', [JadwalSekolahController::class, 'index'])->name('jadwal_sekolah.index');
+        Route::get('create', [JadwalSekolahController::class, 'create'])->name('jadwal_sekolah.create');
+        Route::get('edit', [JadwalSekolahController::class, 'edit'])->name('jadwal_sekolah.edit');
+    });
+
+    Route::prefix('jadwal-kelas')->group(function(){
+        Route::get('', [JadwalKelasController::class, 'index'])->name('jadwal_kelas.index');
+    });
 });
 
 Route::get('/dashboard', function () {
