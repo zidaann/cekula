4f2b742acef773e88a7bfd9e7517718f15fc0fe2
WildanR1 <wildanrachmadoni@gmail.com>
2022-10-23 21:28:26 +0700

    Update Sidebar + Dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -5,14 +5,14 @@
         <div class="mb-5">
             {{-- judul --}}
             <div class="justify-content-center d-flex mb-4">
-                <div class="bg-color d-flex shadow-lg rounded-4 px-3 px-md-5 align-content-center pt-2">
+                <div class="bg-color d-flex shadow-1 rounded-4 px-3 px-md-5 align-content-center pt-2">
                     <h5 class="align-text-bottom px-3 px-md-5 py-1 font-rubik color-user fs-5 text-center">Jadwal Sekolah dan Kelas</h5>
                 </div>
             </div>
             {{-- Content --}}
             <div>
                 <div class="row p-4 justify-content-around">
-                    <div class="col-12 col-md-5 mb-3 bg-color-content rounded-4 shadow">
+                    <div class="col-12 col-md-5 mb-3 bg-color-content rounded-4 shadow-1">
                         <div class="card w-100 bg-transparent border-0">
                             <div class="d-flex justify-content-center">
                                 <img src="/assets/img/dashboard/img1.png" class="card-img-top w-75" >
@@ -23,7 +23,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-12 col-md-5 mb-3 bg-color-content rounded-4 shadow">
+                    <div class="col-12 col-md-5 mb-3 bg-color-content rounded-4 shadow-1">
                         <div class="card w-100 bg-transparent border-0">
                             <div class="d-flex justify-content-center">
                                 <img src="/assets/img/dashboard/img2.png" class="card-img-top w-75" alt="...">
@@ -42,14 +42,14 @@
             <div class="mb-5">
                 {{-- judul --}}
                 <div class="justify-content-center d-flex mb-4">
-                    <div class="bg-color d-flex shadow-lg rounded-4 px-3 px-md-5 align-content-center pt-2">
+                    <div class="bg-color d-flex shadow-1 rounded-4 px-3 px-md-5 align-content-center pt-2">
                         <h5 class="align-text-bottom px-3 px-md-5 py-1 font-rubik color-user fs-5 text-center">Berita Sekolah dan Kelas</h5>
                     </div>
                 </div>
                 {{-- Content --}}
                 <div>
                     <div class="row justify-content-around">
-                        <div class="col-12 col-md-5 mb-3 bg-color-content rounded-4 shadow">
+                        <div class="col-12 col-md-5 mb-3 bg-color-content rounded-4 shadow-1">
                             <div class="card w-100 bg-transparent border-0">
                                 <div class="d-flex justify-content-center">
                                     <img src="/assets/img/dashboard/img3.png" class="card-img-top w-75" alt="...">
@@ -60,7 +60,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12 col-md-5 mb-3 bg-color-content rounded-4 shadow">
+                        <div class="col-12 col-md-5 mb-3 bg-color-content rounded-4 shadow-1">
                             <div class="card w-100 bg-transparent border-0">
                                 <div class="d-flex justify-content-center">
                                     <img src="/assets/img/dashboard/img2.png" class="card-img-top w-75" alt="...">
@@ -79,40 +79,40 @@
         <div class="mb-5">
             {{-- judul --}}
             <div class="justify-content-center d-flex mb-4">
-                <div class="bg-color d-flex shadow-lg rounded-4 px-3 px-md-5 align-content-center pt-2">
+                <div class="bg-color d-flex shadow-1 rounded-4 px-3 px-md-5 align-content-center pt-2">
                     <h5 class="align-text-bottom px-3 px-md-5 py-1 font-rubik color-user fs-5 text-center">Ekstrakulikuler</h5>
                 </div>
             </div>
             {{-- Content --}}
             <div>
                 <div class="row justify-content-center">
-                    <div class="col-6 col-md-4 col-lg-3 bg-color rounded-4 shadow g-0 m-3">
+                    <div class="col-6 col-md-4 col-lg-3 bg-color rounded-4 shadow-1 g-0 m-3">
                         <div class="card w-100 bg-transparent border-0">
                             <div class="d-flex justify-content-center bg-color-content w-100 rounded-4 align-items-center" style="height: 150px;">
-                                <img src="/assets/img/dashboard/img4.png" class="card-img-top w-100 h-100 " alt="...">
+                                <img src="/assets/img/dashboard/Pramuka.png" class="card-img-top w-100 h-100 " alt="...">
                             </div>
                             <div class="card-body text-center color-user">
                                 <h5 class="card-title font-noto fw-semibold">Pramuka</h5>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 col-md-4 col-lg-3 mb-3 bg-color rounded-4 shadow g-0 m-3">
+                    <div class="col-6 col-md-4 col-lg-3 mb-3 bg-color rounded-4 shadow-1 g-0 m-3">
                         <div class="card w-100 bg-transparent border-0">
                             <div class="d-flex justify-content-center bg-color-content w-100 rounded-4 align-items-center img-fluid" style="height: 150px;">
-                                <img src="/assets/img/dashboard/img5.png" class="card-img-top w-100 h-100" alt="...">
+                                <img src="/assets/img/dashboard/PMR.png" class="card-img-top w-100 h-100" alt="...">
                             </div>
                             <div class="card-body text-center color-user">
                                 <h5 class="card-title font-noto fw-semibold">PMR</h5>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 col-md-4 col-lg-3 mb-3 bg-color rounded-4 shadow g-0 m-3">
+                    <div class="col-6 col-md-4 col-lg-3 mb-3 bg-color rounded-4 shadow-1 g-0 m-3">
                         <div class="card w-100 bg-transparent border-0">
                             <div class="d-flex justify-content-center bg-color-content w-100 rounded-4 align-items-center" style="height: 150px;">
-                                <img src="/assets/img/dashboard/img6.png" class="card-img-top w-100 h-100 img-fluid" alt="...">
+                                <img src="/assets/img/dashboard/Olimpiade.png" class="card-img-top w-100 h-100 img-fluid" alt="...">
                             </div>
                             <div class="card-body text-center color-user">
-                                <h5 class="card-title font-noto fw-semibold">Kerohanian</h5>
+                                <h5 class="card-title font-noto fw-semibold">Olimpiade</h5>
                             </div>
                         </div>
                     </div>
