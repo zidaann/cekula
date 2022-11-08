0fe0a4391f12529f5c13fae5661ed9dacb56d3a5
WildanR1 <wildanrachmadoni@gmail.com>
2022-11-08 18:27:14 +0700

    Data Murid + Revisi Rapor

---
+++ resources/views/dashboard/administrasi/_form/form_1.blade.php
@@ -0,0 +1,44 @@
+<div class="mb-3">
+  <label for ="nama_murid" class="form-label"> Masukkan Nama Murid </label>
+  <input type ="text" name="nama_murid" id="nama_murid" class="form-control" placeholder="Masukkan nama murid" >
+</div>
+<div class="mb-3">
+  <label for ="nik" class="form-label"> Nomor Induk Siswa </label>
+  <input type ="text" name="nik" id="nik" class="form-control" placeholder="Masukkan nomor induk siswa">
+</div>
+<div class="mb-3">
+  <label for ="nisn" class="form-label"> NISN </label>
+  <input type="text" name="nisn" id="nisn" class="form-control" placeholder="Masukkan NISN">
+</div>
+<div class="mb-3">
+  <label for ="jenis_kelamin" class="form-label"> Jenis Kelamin </label>
+  <select class="form-select bg-color-content border-0" name="jenis_kelamin" id="jenis_kelamin" placeholder="Pilihan">
+    <option selected>Pilihan</option>
+    <option value="L">Laki-Laki</option>
+    <option value="P">Perempuan</option>
+  </select>
+</div>
+<div class="mb-3">
+  <label for ="tempat_lahir" class="form-label"> Tempat Lahir </label>
+  <input type="text" name="tempat_lahir" id="tempat_lahir" class="form-control" placeholder="Masukkan tempat lahir murid">
+</div>
+<div class="mb-3">
+  <label for ="tgl_lahir" class="form-label"> Tanggal Lahir </label>
+  <input type="date" name="tgl_lahir" id="tgl_lahir" class="form-control bg-color-content border-0" placeholder="Masukkan tanggal lahir murid">
+</div>
+<div class="mb-3">
+  <div class="row">
+    <div class="d-flex align-items-center"><img src="/assets/img/important_form.png" alt="" class="img-fluid me-2" style="width:20px ;">
+      <small class="text-danger"> Masukkan data ini dengan benar!</small>
+    </div>
+  </div>
+</div>
+<div class="d-flex justify-content-end my-4">
+  <div class="font-rubik font-16 fw-semibold" style="color: #9FC3F9;">Halaman 1 dari 4</div>
+</div>
+<div class="d-grid gap-2 col-6 py-2 justify-content-center mx-auto btn-blue rounded">
+    <button class="border-0 d-flex bg-transparent font-rubik font-16 fw-semibold text-white" type="button">
+      <div class="pe-3">Lanjut</div>
+      <i class="bi bi-chevron-right"></i>
+    </button>
+</div>
---
+++ resources/views/dashboard/administrasi/_form/form_3.blade.php
@@ -0,0 +1,44 @@
+<div class="mb-3">
+  <label for ="ortu" class="form-label"> Nama Orang Tua (Ibu/Bapak) </label>
+  <input type ="text" name="ortu" id="ortu" class="form-control" placeholder="Masukkan nama orang tua murid" >
+</div>
+<div class="mb-3">
+  <label for ="pekerjaan_ortu" class="form-label"> Pekerjaan Orang Tua </label>
+  <input type ="text" name="pekerjaan_ortu" id="pekerjaan_ortu" class="form-control" placeholder="Masukkan pekerjaan orang tua">
+</div>
+<div class="mb-3">
+  <label for ="no_ortu" class="form-label"> No. Telepon Orang Tua </label>
+  <input type="text" name="no_ortu" id="no_ortu" class="form-control" placeholder="Masukkan nomor telepon orang tua">
+</div>
+<div class="mb-3">
+  <label for ="wali" class="form-label"> Nama Wali  </label>
+  <input type="text" name="wali" id="wali" class="form-control" placeholder="Masukkan nama wali">
+</div>
+<div class="mb-3">
+  <label for ="pekerjaan_wali" class="form-label"> Pekerjaan Wali </label>
+  <input type ="text" name="pekerjaan_wali" id="pekerjaan_wali" class="form-control" placeholder="Masukkan pekerjaan wali">
+</div>
+<div class="mb-3">
+  <label for ="no_wali" class="form-label"> No. Telepon Wali </label>
+  <input type ="text" name="no_wali" id="no_wali" class="form-control" placeholder="Masukkan nomor telepon wali">
+</div>
+<div class="mb-3">
+  <label for ="no_siswa" class="form-label"> No. Telepon Siswa </label>
+  <input type ="text" name="no_siswa" id="no_siswa" class="form-control" placeholder="Masukkan nomor telepon siswa">
+</div>
+<div class="mb-3">
+  <div class="row">
+    <div class="d-flex align-items-center"><img src="/assets/img/important_form.png" alt="" class="img-fluid me-2" style="width:20px ;">
+      <small class="text-danger"> Masukkan data ini dengan benar!</small>
+    </div>
+  </div>
+</div>
+<div class="d-flex justify-content-end my-4">
+  <div class="font-rubik font-16 fw-semibold" style="color: #9FC3F9;">Halaman 3 dari 4</div>
+</div>
+<div class="d-grid gap-2 col-6 py-2 justify-content-center mx-auto btn-blue rounded">
+    <button class="border-0 d-flex bg-transparent font-rubik font-16 fw-semibold text-white" type="button">
+      <div>Lanjut</div>
+      <i class="bi bi-chevron-right"></i>
+    </button>
+</div>
