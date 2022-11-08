fbf2531c7879e7d8a9f5d7e853c56e2d9ec43e64
WildanR1 <wildanrachmadoni@gmail.com>
2022-11-08 19:53:04 +0700

    Show Jadwal Sekolah

---
+++ resources/views/dashboard/akademik/jadwal_sekolah/show.blade.php
@@ -0,0 +1,36 @@
+@extends('dashboard.main')
+@section('content')
+<div class="header-schedule">
+    <div class="d-flex align-items-center justify-content-center">
+      <span class="text-white">Jadwal Sekolah</span>
+    </div>
+  </div>
+  <main>
+    <div class="mx-5 px-5">
+        {{-- judul --}}
+        <div>
+            <p class="font-rubik font-20">Pentas Seni Akhir Tahun</p>
+        </div>
+        {{-- image --}}
+        <div>
+            <img src="#" alt="#">
+        </div>
+        {{-- tanggal --}}
+        <div class="d-flex align-items-center">
+            <div>
+                <a href="#" class="font-noto font-16 text-decoration-none" style="color: black;">Jadwal Sekolah</a>
+            </div>
+            <div>
+                <i class="bi bi-dot" style="color: #C5DBFB; font-size: 50px;"></i>
+            </div>
+            <div class="font-noto font-16">
+                29 Desember - 30 Desember 2022
+            </div>
+        </div>
+        <div class="font-noto font-16">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia debitis quaerat ullam dolor, mollitia architecto. Veritatis vitae vero mollitia vel explicabo maiores, doloremque rem, earum, aperiam fugiat in itaque unde expedita recusandae magnam tempora. Soluta alias eos libero repudiandae praesentium nesciunt dolor atque blanditiis iure fuga, et ab minus mollitia. Odit sit eos ex, quod possimus aut impedit aliquid accusantium distinctio, optio, doloremque sed facilis nam corrupti cumque? Sapiente necessitatibus facilis recusandae magnam illum velit. Officia doloremque laudantium rem sit neque placeat qui quis! Ex modi commodi expedita asperiores tempora! Voluptatum beatae molestias ullam fuga in rerum veniam, perspiciatis omnis!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni saepe modi beatae minus autem vitae eligendi rem tenetur repudiandae, accusamus quaerat. Sunt voluptate ea qui, eos perspiciatis, reiciendis atque blanditiis voluptates aspernatur fugiat, natus maxime voluptatum delectus! Esse dolorem, suscipit nobis aspernatur dicta amet facilis aperiam corrupti maxime sapiente. Voluptates eius magni pariatur tempora vitae excepturi doloribus iure cupiditate dolore enim maiores ad quas doloremque veniam repellendus, saepe, ipsa, earum qui quo. Tenetur accusantium, quibusdam iusto consequuntur quis accusamus tempore, hic deleniti asperiores cupiditate eligendi dolore a illo esse! Esse, iste. Consequuntur repudiandae mollitia officiis quasi iure quos ad sunt.</p>
+        </div>
+    </div>
+  </main>
+@endsection
