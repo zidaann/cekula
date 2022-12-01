4531664e01cb99763af0504b285ff490a6cef0e8
WildanR1 <wildanrachmadoni@gmail.com>
2022-12-02 03:06:52 +0700

    Revisi Landing Page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,27 +1,27 @@
-<nav class="navbar navbar-expand-lg bg-white py-4 px-4 ">
-    <div class="container-fluid">
+<nav class="navbar navbar-expand-lg bg-white py-4 px-4 fixed-top">
+  <div class="container-fluid">
     <a href=""><img src="/assets/svg/logo.svg" width="150"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item ">
-            <a class="nav-link " aria-current="page" href="#beranda">Beranda</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#tentang">Tentang</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#berita_beranda">Berita</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">PPDB</a>
-          </li>
-          <li class="nav-item nav-login">
-            <a class="nav-link" aria-current="page" href="{{ route('login') }}">Masuk</a>
-          </li>
-        </ul>
-      </div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item ">
+          <a class="nav-link " aria-current="page" href="#beranda">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#tentang">Tentang</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#berita_beranda">Berita</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">PPDB</a>
+        </li>
+        <li class="nav-item nav-login">
+          <a class="nav-link" aria-current="page" href="{{ route('login') }}">Masuk</a>
+        </li>
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
