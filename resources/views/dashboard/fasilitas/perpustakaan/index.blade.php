1395c4776aed15029a0add2585ea0627569db33d
WildanR1 <wildanrachmadoni@gmail.com>
2022-12-09 01:55:44 +0700

    Perpustakaan (Peminjaman Buku)

--- resources/views/dashboard/fasilitas/perpustakaan/index.blade.php
+++ resources/views/dashboard/fasilitas/perpustakaan/index.blade.php
@@ -1,42 +1,41 @@
 @extends('dashboard.main')
-@section('styles')
-    <style>
-        a:hover{
-            color: #797F8F;
-        }
-    </style>
-@endsection
 @section('content')
-<div class="header-schedule">
-    <div class="d-flex align-items-center justify-content-center">
-      <span class="text-white">Perpustakaan</span>
+  <div class="header-schedule shadow-1">
+    <div class="d-flex align-items-center px-4">
+      <span class="text-white mx-auto">Perpustakaan</span>
     </div>
   </div>
-  
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-10 ">
-        <div class="row mt-5 p-3">
-          <a href="#" class="d-flex justify-content-between text-decoration-none text-grey-700 p-2 align-items-center mb-3 rounded box-shadow p-3">
-            <h6>Peminjaman & Pengembalian Buku</h6>
-            <span><i class="bi bi-chevron-right"></i></span>
-          </a>
-          <a href="#" class="d-flex justify-content-between text-decoration-none text-grey-700 p-2 align-items-center mb-3 rounded box-shadow p-3">
-            <h6>Denda Keterlambatan & Hilang</h6>
-            <span><i class="bi bi-chevron-right"></i></span>
-          </a>
-          <a href="#" class="d-flex justify-content-between text-decoration-none text-grey-700 p-2 align-items-center mb-3 rounded box-shadow p-3">
-            <h6>Sumbang Buku</h6>
-            <span><i class="bi bi-chevron-right"></i></span>
-          </a>
-        </div>
+  <div id="main" class="mx-3 px-4">
+    <div class="row px-5">
+      <div class="col-12 p-3 px-4 mb-3 btn btn-light bg-white box-shadow">
+        <a href="#" class="d-flex justify-content-between text-decoration-none text-dark">
+          <span class="font-noto fw-semibold font-16">Peminjaman & Pengembalian Buku</span>
+          <span><i class="bi bi-chevron-right"></i></span>
+        </a>
+      </div>
+      <div class="col-12 p-3 px-4 mb-3 btn btn-light bg-white box-shadow">
+        <a href="#" class="d-flex justify-content-between text-decoration-none text-dark">
+          <span class="font-noto fw-semibold font-16">Denda Keterlambatan & Hilang</span>
+          <span><i class="bi bi-chevron-right"></i></span>
+        </a>
+      </div>
+      <div class="col-12 p-3 px-4 mb-3 btn btn-light bg-white box-shadow">
+        <a href="#" class="d-flex justify-content-between text-decoration-none text-dark">
+          <span class="font-noto fw-semibold font-16">Sumbang Buku</span>
+          <span><i class="bi bi-chevron-right"></i></span>
+        </a>
+      </div>
+      <div class="col-12 mb-3 d-flex justify-content-center">
+        <figure>
+          <img src="/assets/img/dashboard/perpustakaan/ilustrasi_perpustakaan.svg" alt="Perpustakaan">
+          <figcaption class="font-noto font-16 text-center">Pilih menu perpustakaan terlebih dahulu</figcaption>
+        </figure>
       </div>
     </div>
-    <div class="row mt-5">
-      <div class="col-12 col-sm-12 py-4 text-center text-grey-600 font-noto">
-        <img src="/assets/img/dashboard/perpustakaan/ilustrasi_perpustakaan.png" class="img-fluid" width="350">
-        <h6 class="mt-3">Pilih menu perpustakaan terlebih dahulu</h6>
-      </div>
   </div>
+  <div class="btn-add">
+    <a href="#">
+      <img src="/assets/img/dashboard/btn_add.png" class="img-fluid">
+    </a>
   </div>
 @endsection
